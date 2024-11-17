ef35484a8dccb9eec13a90a0c6ea7faef9947c9a
Miguel Sang <miguela.sangc@konradlorenz.edu.co>
2024-11-16 23:00:03 -0500

    correcion de versiones

--- routes/estadisticasRoutes.js
+++ routes/estadisticasRoutes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 const estadisticasController = require('../controllers/estadisticasController');
 
+// Ruta para crear o actualizar estadísticas
 router.post('/', estadisticasController.actualizarEstadistica);
+
+// Ruta para obtener todas las estadísticas
 router.get('/', estadisticasController.obtenerEstadisticas);
 
+// Ruta para actualizar estadísticas (si es necesario tener una ruta PUT separada)
+router.put('/:id', estadisticasController.actualizarEstadistica);
+
 module.exports = router;
-// Ruta para crear estadísticas
-router.post('/', estadisticasController.crearEstadistica);
-// Ruta para actualizar estadísticas
-router.put('/', estadisticasController.actualizarEstadistica);
